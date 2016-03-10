6c66cebc4ea78ed89bf1fe2a29fb0c9e930990b7
Nat14 <naryucha14@gmail.com>
2016-03-09 20:19:22 -0800

    updated getting data from controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,4 +2,21 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
+    person1 = {
+      name: 'Tim',
+      email: 'a@b.com',
+      number: '123-345-5678'
+    };
+    person2 = {
+      name: 'Pim',
+      email: 'a@b.com',
+      number: '123-345-5678'
+    };
+    person3 = {
+      name: 'Dim',
+      email: 'a@b.com',
+      number: '123-345-5678'
+    };
+    var contactlist = [person1, person2, person3];
+    $scope.contactlist = contactlist;
 }]);﻿
