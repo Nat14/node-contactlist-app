b16921a0e16b6d6c2577e8b87c78b8b013f73cd3
Nat14 <naryucha14@gmail.com>
2016-03-10 08:54:10 -0800

    added getting data from database

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,21 +2,39 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
+    // this part is for getting data from server - data in database
+    $http.get('/contactlist_d').success(function(response){
+      console.log("I got the data I requested");
+
+      $scope.contactlist_d = response;
+    });
+    // end part
+
+    // this part is for getting data from server - data in server.js
+    $http.get('/contactlist').success(function(response){
+      console.log("I got the data I requested");
+
+      $scope.contactlist_s = response;
+    });
+    // end part
+
+    // this part is used to show data from controller in index.html
     person1 = {
-      name: 'Tim',
+      name: 'Cat',
       email: 'a@b.com',
       number: '123-345-5678'
     };
     person2 = {
-      name: 'Pim',
+      name: 'Cole',
       email: 'a@b.com',
       number: '123-345-5678'
     };
     person3 = {
-      name: 'Dim',
+      name: 'Cassidy',
       email: 'a@b.com',
       number: '123-345-5678'
     };
     var contactlist = [person1, person2, person3];
-    $scope.contactlist = contactlist;
+    // $scope makes it posible to use this variable in view (ex. index.html)
+    $scope.contactlist_c = contactlist;
 }]);﻿
--- server.js
+++ server.js
@@ -1,7 +1,45 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 app.use(express.static(__dirname + '/public'));
+
+// this part is used to send data from database back to controller
+app.get('/contactlist_d', function (req, res) {
+  console.log('I received a GET request');
+
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+// end part
+
+// this part is used to send data back to controller
+app.get('/contactlist', function(req, res) {
+  console.log("I received a GET request");
+
+person1 = {
+  name: 'Sam',
+  email: 'a@b.com',
+  number: '123-345-5678'
+};
+person2 = {
+  name: 'Sonny',
+  email: 'a@b.com',
+  number: '123-345-5678'
+};
+person3 = {
+  name: 'Soda',
+  email: 'a@b.com',
+  number: '123-345-5678'
+};
+var contactlist = [person1, person2, person3];
+
+res.json(contactlist);
+});
+// end part
 
 var server = app.listen(3000, function () {
 
